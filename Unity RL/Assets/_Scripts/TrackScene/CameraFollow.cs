ceb83e5ca9018aa867f29ef2b8753d594666f56a
Nicholas Wile <74445404+nicholaswile@users.noreply.github.com>
2024-12-01 23:20:30 -0500

    fixed cameras for real this time

--- Unity RL/Assets/_Scripts/TrackScene/CameraFollow.cs
+++ Unity RL/Assets/_Scripts/TrackScene/CameraFollow.cs
@@ -10,10 +10,36 @@ public class CameraFollow : MonoBehaviour
     private Transform _target;
 
     [SerializeField]
-    private Vector3 _offset;
+    private List<Transform> _locations;
+    private Transform _camera_location;
+
+    [SerializeField]
+    private float _smoothing = 0.5f;
+
+    private int _location_index = 0;
+
+    private void Awake()
+    {
+        _camera_location = _locations[0];
+    }
 
     private void Update()
     {
-        transform.localPosition = _target.localPosition + _offset;
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            _location_index = (_location_index + 1)%_locations.Count;
+        }
+
+        _camera_location = _locations[_location_index];
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 change = _camera_location.position * (1-_smoothing) + transform.position * _smoothing;
+
+        transform.position = new Vector3(change.x, transform.position.y, change.z);
+
+        transform.LookAt(_target);
+
     }
 }
